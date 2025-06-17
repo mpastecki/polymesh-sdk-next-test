2ede109e97685b24229eac8e8debc9dc2ff02e11
Prashant Bajpai <34747455+prashantasdeveloper@users.noreply.github.com>
2025-06-17 18:44:40 +0530

    test: 💍 fix ballot tests

--- src/testUtils/mocks/dataSources.ts
+++ src/testUtils/mocks/dataSources.ts
@@ -5027,6 +5027,6 @@ export const createMockCorporateBallotMeta = ({
       title: formattedTitle,
       motions: formattedMotions,
     },
-    !{ title, motions }
+    !title && !motions
   );
 };
